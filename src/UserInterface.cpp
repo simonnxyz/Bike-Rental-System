3a5fc46988eeb56a8e95841633bc43bca6883f46
mpedziwi <01187270@pw.edu.pl>
2024-05-02 13:04:18 +0200

    changes to print_starting_menu

--- src/UserInterface.cpp
+++ src/UserInterface.cpp
@@ -10,23 +10,24 @@ void UserInterface::print_starting_text(int width, std::string text_color, std::
     print_char(' ', (width - title_text.length())/2, false);
     std::cout << get_color_code(true, text_color) << title_text << get_color_code(false, "reset");
     std::cout << std::endl << std::endl << get_color_code(false, "reset");
-    print_starting_menu(width, text_color, border_color);
 }
 
 void UserInterface::print_starting_menu(int width, std::string text_color, std::string border_color) const
 {
-    std::string option_1 = "1: >Pierwsza Opcja<";
-    std::string option_2 = "2: >Druga Opcja<";
-    std::string option_3 = "3: >Trzecia Opcja<";
-    std::string option_4 = "4: >Czwarta Opcja<";
-    std::string tbc = "...";
+    std::string option_1 = "1: >Wybierz punkt wypożyczenia<";
+    std::string option_2 = "2: >Wyświetl saldo<";
+    std::string option_3 = "3: >Doładuj środki<";
+    std::string option_4 = "4: >Wyświetl informacje o koncie<";
+    std::string option_5 = "5: >Wyświetl historię wypożyczeń<";
+    std::string option_6 = "6: >Wyloguj się";
 
     std::vector<std::string> options;
     options.push_back(option_1);
     options.push_back(option_2);
     options.push_back(option_3);
     options.push_back(option_4);
-    options.push_back(tbc);
+    options.push_back(option_5);
+    options.push_back(option_6);
 
     print_options(options, width, text_color);
 
@@ -37,7 +38,9 @@ void UserInterface::print_starting_menu(int width, std::string text_color, std::
 
 void UserInterface::redirect_from_starting_menu(std::string text_color, std::string border_color) const
 {
+    std::cout << get_color_code(true, "cyan");
     int choice = get_user_int_input("Wybierz opcję");
+    std::cout << get_color_code() << std::endl;
 
 
     // Szkielet funkcji, jak już będzie wiadomo co i jak można w miejsca wyjść konsoli wrzucić odpowiednie
