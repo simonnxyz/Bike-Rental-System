3df0c33862f6f7eb8e24f1b959abb9a67fd963f3
Wejtek <karol.mezenski@gmail.com>
2024-05-16 20:35:32 +0200

    Restored previous constructor due to issues with const double &balance = 0.00

--- include/User.h
+++ include/User.h
@@ -8,7 +8,9 @@ public:
   // constructors
   User() = default;
   User(const std::string &user_name, const std::string &user_email,
-       const std::string &password, const double &balance = 0.00);
+       const std::string &password, const double &balance);
+  User(const std::string &user_name, const std::string &user_email,
+       const std::string &password);
 
   // virtuals
   virtual void load(const std::string &data) override;
